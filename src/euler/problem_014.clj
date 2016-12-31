e73612d5d9d1ff6f30f77c9d274d983e1bc55ffa
Michael Cornelius <kouneriasu@gmail.com>
2016-12-30 21:40:39 -0600

    Problem 14: performance improvement
    Memoize calculation of Collatz sequence length.

--- src/euler/problem_014.clj
+++ src/euler/problem_014.clj
@@ -3,10 +3,18 @@
 (defn collatz-next [n]
   (if (even? n) (/ n 2) (inc (* 3 n))))
 
+(def memo (atom {}))
+
 (defn collatz-len [n]
-  (loop [n n len 1]
-    (if (= n 1) len
-        (recur (collatz-next n) (inc len)))))
+  (letfn [(result [len chain]
+            (apply swap! memo assoc (interleave chain (range len 0 -1)))
+            len)]
+    (loop [m n len 0 chain []]
+      (if-let [e (find @memo m)]
+        (result (+ len (val e)) (conj chain m))
+        (if (= m 1)
+          (result (inc len) chain)
+          (recur (collatz-next m) (inc len) (conj chain m)))))))
 
 (defn solution []
   (first
