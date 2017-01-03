e5ff65c4a3e3e048dfd52f4e857137f16e6b8cb0
Michael Cornelius <michael@dsndata.com>
2017-01-03 15:44:26 -0600

    Simplify.
    By initializing the memo with the 1 -> 1 mapping, we can avoid having to
    handle n = 1 explicitly, and we don't need to define and call the
    internal "result" function, since we are only performing the operation
    in one branch.

--- src/euler/problem_014.clj
+++ src/euler/problem_014.clj
@@ -3,18 +3,16 @@
 (defn collatz-next [n]
   (if (even? n) (/ n 2) (inc (* 3 n))))
 
-(def memo (atom {}))
+(def memo (atom {1 1}))
 
 (defn collatz-len [n]
-  (letfn [(result [len chain]
-            (apply swap! memo assoc (interleave chain (range len 0 -1)))
-            len)]
-    (loop [m n len 0 chain []]
-      (if-let [e (find @memo m)]
-        (result (+ len (val e)) (conj chain m))
-        (if (= m 1)
-          (result (inc len) chain)
-          (recur (collatz-next m) (inc len) (conj chain m)))))))
+  (loop [m n len 0 chain []]
+    (if-let [e (find @memo m)]
+      (let [len (+ len (val e))]
+        (when (not-empty chain)
+          (apply swap! memo  assoc (interleave chain (range len 0 -1))))
+        len)
+      (recur (collatz-next m) (inc len) (conj chain m)))))
 
 (defn solution []
   (first
